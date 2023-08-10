c4bb093a36b98cb4177472b369b1d06d3f1ad796
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 21:46:43 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import { take } from 'rxjs/operators';
 import { interval, zip } from 'rxjs';
 
 // Example 2: zip when 1 observable completes
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/zip
+
 //emit every 1s
-const source = interval(1000);
+const srcInterval = interval(1000);
 //when one observable completes no more values will be emitted
-const example = zip(source, source.pipe(take(2)));
+const example = zip(srcInterval, srcInterval.pipe(take(2)));
 //output: [0,0]...[1,1]
 const subscribe = example.subscribe((val) => console.log(val));
